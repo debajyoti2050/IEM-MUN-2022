21a96ddcaa3342cde7f448db2e9cc0a934bfe840
prajak <prajaksen.s@gmail.com>
2022-08-12 02:00:42 +0530

    timer commit

--- src/components/importantDates/Dates.jsx
+++ src/components/importantDates/Dates.jsx
@@ -1,8 +1,9 @@
 import './dates.css'
 
 import React from 'react'
-
+import CountDownTimer from './CountDownTimer';
 export default function Dates() {
+  const hoursMinSecs = {hours:1, minutes: 20, seconds: 40}
   return (
     <section id="call-to-action" className="features">
     <div className="container" data-aos="fade-up">
@@ -20,12 +21,9 @@ export default function Dates() {
               <div className="feature-box align-items-center text-center">
                 <h3>OCTOBER</h3>
                 <h3 >&nbsp;14th, 15th & 16th</h3>
-                <>
-                <script src="https://cdn.logwork.com/widget/countdown.js"></script>
-                <a href="https://logwork.com/countdown-w2kb" className="countdown-timer" data-width="684"
-                  data-timezone="Asia/Kolkata" data-textcolor="#ffffff" data-date="2022-10-14 08:00"
-                  data-background="#18d26e" data-digitscolor="#ffffff" data-unitscolor="#ffffff">Time Left</a>
-                  </>
+                <div classname="App">
+                <CountDownTimer hoursMinSecs={hoursMinSecs}/>
+                  </div>
               </div>
             </div>
 
