985ff4f5ef6b43ba92e7c727d392d05de6583bf9
Pritam Das <pritamstudy123@gmail.com>
2022-08-12 01:44:59 +0530

    navbar

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,13 +5,14 @@ import Contact from "../components/contact/Contact";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import Hero from "../components/hero/Hero";
+import Navbar from "../components/navbar/navbar";
 import Dates from "../components/importantDates/Dates";
 
 export default function Homepage() {
   return (
     <>
       <main id="main">
-        <Header />
+        <Navbar />
         <Hero />
         <Dates />
         <About />
