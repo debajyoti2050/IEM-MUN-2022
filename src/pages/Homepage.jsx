7b08828f3826e80875fa46135e4e3f81e9f63506
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-08-14 00:52:14 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,7 +4,6 @@ import Collaborators from "../components/collaboration/Collaborators";
 import Committee from "../components/committee/Committee";
 import Contact from "../components/contact/Contact";
 import Footer from "../components/footer/Footer";
-import Header from "../components/header/Header";
 import Hero from "../components/hero/Hero";
 import Navbar from "../components/navbar/navbar";
 import Dates from "../components/importantDates/Dates";
