6efc3c7330be8610a1b96b7023d7f757822c8fd4
Pritam Das <pritamstudy123@gmail.com>
2022-08-19 10:18:38 +0530

    form

--- src/components/applyform/components/TextField.jsx
+++ src/components/applyform/components/TextField.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import './signup.css'
 import { ErrorMessage, useField } from 'formik';
 
 export const TextField = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
     <div className="mb-2">
-      <label htmlFor={field.name}>{label}</label>
+      <label htmlFor={field.name}><h5>{label}</h5></label>
       <input
         className={`form-control shadow-none ${meta.touched && meta.error && 'is-invalid'}`}
         {...field} {...props}
         autoComplete="off"
       />
-      <ErrorMessage component="div" name={field.name} className="error" />
+      <span><ErrorMessage component="div" name={field.name} className="error" /></span>
     </div>
   )
 }
