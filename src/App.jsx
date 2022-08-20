497dda3d315504b40e7755c680e5da9a8b9c8671
chaitak-gorai <chaitakgorai@gmail.com>
2022-08-20 10:38:46 +0530

    Pages updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,58 @@
-
-import React, { Component ,useState , useEffect } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import './App.css'
 import Homepage from './pages/Homepage'
 import Forms from './components/applyform/FinalForm'
 import Committees from './components/eachcommittee/Committeedesc'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import GridLoader from "react-spinners/GridLoader";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import GridLoader from 'react-spinners/GridLoader'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 import PaymentSuccess from './pages/payment-success/PaymentSuccess'
 import PaymentFailed from './pages/payment-failed/PaymentFailed'
 
 function App() {
-
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    setLoading(true);
+    setLoading(true)
     setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
+      setLoading(false)
+    }, 3000)
+  }, [])
 
   return (
-    <div className="APP">
-     {loading ? (
-      <div className="munloader">
-      <GridLoader
-
-      className="loader"
-        color={"#44CE16"}
-        // css={override}
-        loading={loading}
-        size={30}
-      />
-      </div>
-     
-    ):(
-
-
-    <Router>
-      <Switch>
-        <Route exact path='/'><Homepage/></Route>
-        <Route path='/form'><Forms/></Route>
-        <Route path='/committeedesc'><Committees/></Route>
-        <Route exact path='/payment-success'><PaymentSuccess/></Route>
-        <Route exact path='/payment-failed'><PaymentFailed/></Route>
-      </Switch>
-    </Router>
-    )}
+    <div className='APP'>
+      {loading ? (
+        <div className='munloader'>
+          <GridLoader
+            className='loader'
+            color={'#44CE16'}
+            // css={override}
+            loading={loading}
+            size={30}
+          />
+        </div>
+      ) : (
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Homepage />
+            </Route>
+            <Route path='/form'>
+              <Forms />
+            </Route>
+            <Route path='/committeedesc'>
+              <Committees />
+            </Route>
+            <Route exact path='/registraion-success'>
+              <PaymentSuccess />
+            </Route>
+            <Route exact path='/registration-failed'>
+              <PaymentFailed />
+            </Route>
+          </Switch>
+        </Router>
+      )}
     </div>
     // <BrowserRouter>
     //   <Routes>
@@ -64,7 +63,6 @@ function App() {
     //   </Routes>
     // </BrowserRouter>
   )
-
 }
 
 export default App
--- src/pages/payment-success/PaymentSuccess.jsx
+++ src/pages/payment-success/PaymentSuccess.jsx
@@ -2,30 +2,34 @@ import './paymentsuccess.css'
 
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {Check2} from 'react-bootstrap-icons'
-
-
+import { Check2 } from 'react-bootstrap-icons'
 
 export default function PaymentSuccess() {
   return (
-    <div className="container">
-   <div className="row">
-      <div className="col-md-6 mx-auto mt-5">
-         <div className="payment">
-            <div className="successpayment_header">
-               <div className="check"><i className="fa fa-check" aria-hidden="true"><Check2/></i></div>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6 mx-auto mt-5'>
+          <div className='payment'>
+            <div className='successpayment_header'>
+              <div className='check'>
+                <i className='fa fa-check' aria-hidden='true'>
+                  <Check2 />
+                </i>
+              </div>
             </div>
-            <div className="successcontent">
-               <h1>Payment Success !</h1>
-               <p>Thank You for your registration in IEM MUN 2022.Please check your mail for the confirmation. </p>
-               <Link to='/'>
-               <a href="/">Go to Home</a>
-               </Link>
+            <div className='successcontent'>
+              <h1>Registration Success !</h1>
+              <p>
+                Thank You for your registration in IEM MUN 2022.Please check
+                your mail for the confirmation.{' '}
+              </p>
+              <Link to='/'>
+                <a href='/'>Go to Home</a>
+              </Link>
             </div>
-            
-         </div>
+          </div>
+        </div>
       </div>
-   </div>
-</div>
+    </div>
   )
 }
