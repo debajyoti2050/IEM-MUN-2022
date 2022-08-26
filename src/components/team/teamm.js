58e1d7152cf22c5279a2ca55021bbb3ca2ef6bf9
PRAJAK SEN <80170713+prajak002@users.noreply.github.com>
2022-08-26 22:19:57 +0530

    team

--- src/components/team/teamm.js
+++ src/components/team/teamm.js
@@ -1,10 +1,146 @@
 const Members=[
     {
         id:1,
-        // image:"../images/team/Avinash.jpg",
-        Name:"",
-        team:""
+        image:"../images/team/Avinash.jpg",
+        Name:"Ahana Mukherjee",
+        team:"PR TEAM"
     },
+    {
+        id:2,
+        image:"../images/team/Avinash.jpg",
+        Name: "Soubham Singha",
+        team: "PR TEAM"
+    },
+    {
+         id:3,
+        image:"../images/team/Avinash.jpg",
+        Name: "Ritika Paul",
+        team: "Graphics"
+    },
+    {
+         id:4,
+        image:"../images/team/Avinash.jpg",
+        Name: "Raihan Khan",
+        team: "Graphics"
+    },
+    
+        {
+         id:5,
+        image:"../images/team/Avinash.jpg",
+        Name: "Sayantan Pal",
+        team: "Content"
+    },
+    {
+         id:6,
+        image:"../images/team/Avinash.jpg",
+        Name: "Soumyanath Tripathy",
+        team: "Socials"
+    },
+    {
+         id:7,
+        image:"../images/team/Avinash.jpg",
+        Name: "Nikhil Anand",
+        team: "Socials"
+    },
+    {
+         id:8,
+        image:"../images/team/Avinash.jpg",
+        Name: "Debjani Dey",
+        team: "Tech"
+    },
+    {
+         id:9,
+        image:"../images/team/Avinash.jpg",
+        Name: "Shagil Islam",
+        team: "Graphics"
+    },
+    {
+         id:10,
+        image:"../images/team/Avinash.jpg",
+        Name: "Anaranya Bose",
+        team: "Outreach"
+    },
+    {
+         id:11,
+        image:"../images/team/Avinash.jpg",
+        Name: "Debarghya Roy",
+        team: "Outreach"
+    },
+    {
+         id:12,
+        image:"../images/team/Avinash.jpg",
+        Name: "Avinash Chourasia",
+        team: "Logistics"
+    },
+    {
+         id:13,
+        image:"../images/team/Avinash.jpg",
+        Name: "Aman Kumar",
+        team: "Logistics"
+    },
+    {
+         id:14,
+        image:"../images/team/Avinash.jpg",
+        Name: "Ritaja Maitra",
+        team: "Collab"
+    },
+    {
+         id:15,
+        image:"../images/team/Avinash.jpg",
+        Name: "Debaleena Ghatak",
+        team: "Collab"
+    },
+    {
+         id:16,
+        image:"../images/team/Avinash.jpg",
+        Name: "Arnak Dandapat",
+        team: "Sponsorship"
+    },
+   {
+         id:17,
+        image:"../images/team/Avinash.jpg",
+        Name: "Pranay Saha",
+        team: "sponsorship"
+    }, 
+    {
+         id:18,
+        image:"../images/team/Avinash.jpg",
+        Name: "Debayudh Mitra",
+        team: "Delegate Affairs"
+    },
+    {
+         id:19,
+        image:"../images/team/Avinash.jpg",
+        Name: "Prakash Tomar",
+        team: "Delegate Affairs"
+    },
+    {
+         id:20,
+        image:"../images/team/Avinash.jpg",
+        Name: "suchita malik",
+        team: "Hospitality"
+    },
+    
+     {
+         id:21,
+        image:"../images/team/Avinash.jpg",
+        Name: "Souradeep Atar",
+        team: "Hospitality"
+    },
+    
+     {
+         id:22,
+        image:"../images/team/Avinash.jpg",
+        Name: "Arkaprava Paul",
+        team: "Media Coverage"
+    },
+     {
+         id:23,
+        image:"../images/team/Avinash.jpg",
+        Name: "Sneha Biswas",
+        team: "Ashram coordinator"
+    },
+    
     
 ]
 
