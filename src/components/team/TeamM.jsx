6253db9ecc0a1a074f140568b44f4493358614bd
Pritam Das <pritamstudy123@gmail.com>
2022-08-26 21:49:16 +0530

    team

--- src/components/team/TeamM.jsx
+++ src/components/team/TeamM.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "./team.css"
+import Noavatar from '../../assets/noAvatar.png'
 
 const TeamM = (props) => {
   return (
     <div className="col-lg-3 mt-3">
         <div className="shape">
-        <div className="imgcenter"><img src={props.image} className="profile-a"/></div>
+        <div className="imgcenter"><img src={props.image || Noavatar} className="profile-a"/></div>
             <div className="info">
                 <h3>{props.name}</h3>
+                <h4>-{props.lead}</h4>
             </div>
         </div>
     </div>
--- src/components/team/teamm.js
+++ src/components/team/teamm.js
@@ -2,23 +2,10 @@ const Members=[
     {
         id:1,
         image:"../images/team/Avinash.jpg",
-        Name:"Avinash Chourasia"
+        Name:"",
+        team:""
     },
-    {
-        id:2,
-        image:"../images/team/Raihan.jpeg",
-        Name:"Raihan Khan"
-    },
-    {
-        id:3,
-        image:"../images/team/Sayantan.jpg",
-        Name:"Sayantan Pal"
-    },
-    {
-        id:4,
-        image:"../images/team/Prakash.JPG",
-        Name:"Prakash VS Tomar"
-    }
+    
 ]
 
 export default Members
