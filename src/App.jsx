f3fbe06253e2fcfc242c389bd08997cf5f076de5
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-09-18 14:09:13 +0530

    gform

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import React, { Component, useState, useEffect } from 'react'
-import './App.css'
-import Homepage from './pages/Homepage'
-import Forms from './components/applyform/FinalForm'
-import Referral from './components/applyform/ReferForm'
-import Committees from './components/eachcommittee/Committeedesc'
-import Team from './components/team/Team'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import GridLoader from 'react-spinners/GridLoader'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import React, { Component, useState, useEffect } from "react";
+import "./App.css";
+import Homepage from "./pages/Homepage";
+import Forms from "./components/applyform/FinalForm";
+import Referral from "./components/applyform/ReferForm";
+import Committees from "./components/eachcommittee/Committeedesc";
+import Team from "./components/team/Team";
+import "bootstrap/dist/css/bootstrap.min.css";
+import GridLoader from "react-spinners/GridLoader";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import PaymentSuccess from './pages/payment-success/PaymentSuccess'
-import PaymentFailed from './pages/payment-failed/PaymentFailed'
+import PaymentSuccess from "./pages/payment-success/PaymentSuccess";
+import PaymentFailed from "./pages/payment-failed/PaymentFailed";
 
 function App() {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     setTimeout(() => {
-      setLoading(false)
-    }, 3000)
-  }, [])
+      setLoading(false);
+    }, 3000);
+  }, []);
 
   return (
-    <div className='APP'>
+    <div className="APP">
       {loading ? (
-        <div className='munloader'>
+        <div className="munloader">
           <GridLoader
-            className='loader'
-            color={'#44CE16'}
+            className="loader"
+            color={"#44CE16"}
             // css={override}
             loading={loading}
             size={30}
@@ -37,27 +37,34 @@ function App() {
       ) : (
         <Router>
           <Switch>
-            <Route exact path='/'>
+            <Route exact path="/">
               <Homepage />
             </Route>
-            <Route path='/form'>
+            <Route path="/form">
               <Forms />
             </Route>
-            <Route path='/referral'>
+            <Route path="/referral">
               <Referral />
             </Route>
-            <Route path='/committeedesc'>
+            <Route path="/committeedesc">
               <Committees />
             </Route>
-            <Route exact path='/registration-success'>
+            <Route exact path="/registration-success">
               <PaymentSuccess />
             </Route>
-            <Route path='/team'>
+            <Route path="/team">
               <Team />
             </Route>
-            <Route exact path='/registration-failed'>
+            <Route exact path="/registration-failed">
               <PaymentFailed />
             </Route>
+            <Route
+              path="/alternateform"
+              component={() => {
+                window.location.href = "https://forms.gle/5sUkr11zH6hS4ptUA";
+                return null;
+              }}
+            />
           </Switch>
         </Router>
       )}
@@ -70,7 +77,7 @@ function App() {
     //     <Route exact path='/paymentfailed' element={<PaymentFailed />} />
     //   </Routes>
     // </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,57 +1,70 @@
-import './hero.css'
-import HeroBG from '../../assets/BG_image.png'
-import React, { useEffect } from 'react'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { Link } from 'react-router-dom'
+import "./hero.css";
+import HeroBG from "../../assets/BG_image.png";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
 
 export default function Hero() {
+  const history = useHistory();
   useEffect(() => {
-    AOS.init()
-    AOS.refresh()
-  }, [])
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
-    <section id='hero'>
-      <div className='hero-container'>
+    <section id="hero">
+      <div className="hero-container">
         <div
-          id='heroCarousel'
-          className='carousel slide carousel-fade'
-          data-bs-ride='carousel'
-          data-bs-interval='5000'
+          id="heroCarousel"
+          className="carousel slide carousel-fade"
+          data-bs-ride="carousel"
+          data-bs-interval="5000"
         >
           <ol
-            id='hero-carousel-indicators'
-            className='carousel-indicators'
+            id="hero-carousel-indicators"
+            className="carousel-indicators"
             hidden
           ></ol>
 
-          <div className='carousel-inner' role='listbox'>
+          <div className="carousel-inner" role="listbox">
             <div
-              className='carousel-item active'
-              style={{ backgroundImage: 'url(' + HeroBG + ')' }}
+              className="carousel-item active"
+              style={{ backgroundImage: "url(" + HeroBG + ")" }}
             >
-              <div className='carousel-container'>
-                <div className='container'>
+              <div className="carousel-container">
+                <div className="container">
                   <h2
-                    className='animate__animated animate__fadeInDown'
-                    data-aos='fade-down'
-                    data-aos-delay='400'
+                    className="animate__animated animate__fadeInDown"
+                    data-aos="fade-down"
+                    data-aos-delay="400"
                   >
-                    <img height='300rem' src='MUN_Logo.png' alt='mun logo' />
+                    <img height="300rem" src="MUN_Logo.png" alt="mun logo" />
                   </h2>
-                  <h2 data-aos='fade-up' data-aos-delay='400'>
+                  <h2 data-aos="fade-up" data-aos-delay="400">
                     MODEL UNITED NATIONS
                   </h2>
-                  <Link to='/form'>
-                    <a
-                      // onClick={() => {
-                      //   alert('Registration Starts on 20th August at 2PM')
-                      // }}
-                      className='btn-get-started scrollto animate__animated animate__fadeInUp'
-                    >
-                      Register Now
-                    </a>
-                  </Link>
+
+                  <a
+                    onClick={async () => {
+                      try {
+                        await axios
+                          .get("https://cema-mun.herokuapp.com/api/healthcheck")
+                          .then((res) => {
+                            if (!res.status === 200) {
+                              history.push("/form");
+                            } else {
+                              history.push("/alternateform");
+                            }
+                          });
+                      } catch (err) {
+                        history.push("/alternateform");
+                      }
+                    }}
+                    className="btn-get-started scrollto animate__animated animate__fadeInUp"
+                  >
+                    Register Now
+                  </a>
                 </div>
               </div>
             </div>
@@ -59,5 +72,5 @@ export default function Hero() {
         </div>
       </div>
     </section>
-  )
+  );
 }
