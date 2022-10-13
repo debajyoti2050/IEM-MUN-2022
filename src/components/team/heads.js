fc5ca911bde5feecd6e97bc33d51e25f844848a7
Pritam Das <pritamstudy123@gmail.com>
2022-10-13 09:54:44 +0530

    secretariats change

--- src/components/team/heads.js
+++ src/components/team/heads.js
@@ -3,13 +3,13 @@ const Members=[
         id:1,
         image:"../images/team/vedant.jpg",
         Name:"Vedant Parasrampura",
-        team:"General Secretary"
+        team:"Secretary General"
     },
     {
         id:2,
         image:"../images/team/Chaitak_Gorai.jpg",
         Name: "Chaitak Gorai",
-        team: "Deputy General Secretary"
+        team: "Deputy Secretary General"
     },
     {
          id:3,
